b63242db107b1fc6c10545fc280cc665c118abdd
Saloni Goyal <46787992+SaloniGoyal1@users.noreply.github.com>
2020-07-04 11:49:04 +0530

    Display Details

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: [{"item_id":"284","item_name":"Samosa","category_name":"Snack  (Veg)","is_veg":"1","description":"Samosa","price":"28.58"},
+            {"item_id":"285","item_name":"Veg-Cutlet (2pcs)","category_name":"Starters  (Veg)","is_veg":"1","description":"Sweet and spicy mix veg balls with a crispy exterior is the proof Bengalis are not just the ace of spades of sweets","price":"50.00"},
+            {"item_id":"286","item_name":"Dubey Bites","category_name":"All Day Breakfast  (Veg)","is_veg":"1","description":"Nothing better than enjoying sumptuous food from Banaras 3 stuffed matar poori with aloo matar subzi while listening to Bhojpuri songs","price":"125.00"},
+            {"item_id":"287","item_name":"Pav Bhaji","category_name":"Starters  (Veg)","is_veg":"1","description":"Mumbaiyan Diaries are incomplete without Pav Bhaji Fast food dish from Maharashtra consisting of a thick vegetable curry prepared in butter and served with 2pcs of pav chopped onion and lemon slice","price":"45.00"},
+            {"item_id":"300","item_name":"Lays Chips","category_name":"Snack  (Veg)","is_veg":"1","description":"Lays Potato Chips","price":"10.00"},
+            {"item_id":"1057","item_name":"Bread-Omelette Masala (2 Slices)","category_name":"Daily Meal  (Veg)","is_veg":"1","description":"Bread & Omelette Masala (2 Slices)","price":"35.00"},
+            {"item_id":"1058","item_name":"Chapati Mini Meal Veg","category_name":"Daily Meal  (Veg)","is_veg":"1","description":"Chapati Mini Meal Veg","price":"42.85"},
+            {"item_id":"305","item_name":"Sorshe Macher Jhal","category_name":"Main course (Non Veg)","is_veg":"0","description":"Our signature fish Rohu curry made with creamy Bengal style mustard paste green chillies and freshly grounded spices so that you can cheer for one more thing among mishti doi Mithun da and Mohan Bagan","price":"175.00"},
+            {"item_id":"1094","item_name":"Masala Peanut","category_name":"Daily Meal  (Veg)","is_veg":"1","description":"Masala Peanut","price":"38.09"},
+            {"item_id":"322","item_name":"Butter Roti (10 Pcs)","category_name":"Roti(Veg)","is_veg":"1","description":"Butter Roti (10 Pcs)","price":"50.00"},
+            {"item_id":"324","item_name":"Matar Poori with Aloo Subji","category_name":"Daily Meal  (Veg)","is_veg":"1","description":"Enjoy our Banarasi style soft stuffed matar poori which is a treat to the tongue with every bite (3 Pcs)","price":"55.00"},
+            {"item_id":"326","item_name":"Rajdhani  Chole Wrap","category_name":"Snack  (Veg)","is_veg":"1","description":"Nothing better than filling of delectable Dilli style chhole with onion slices topped with mayo and tomato imli chutney wraped in a tortilla","price":"80.00"},
+            {"item_id":"329","item_name":"Peshawari Wrap","category_name":"Make your Wraps (Non Veg)","is_veg":"0","description":"Chicken chunks dipped in creamy cheese and rolled in tortilla is an ode to Peshawari Food tales","price":"110.00"},
+            {"item_id":"331","item_name":"Gulab Jamun","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Made with pure khova this mouth melting Gulabjamun is a sheer ecstacy that ends your meal with tasty delight","price":"20.00"},
+            {"item_id":"332","item_name":"Egg Maggi","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"With its smoky flavour and silky sweet taste this milk condensed to perfection can be best gift for sweet cravings which you cant have just one","price":"45.00"},
+            {"item_id":"337","item_name":"Coke (250 Ml)","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Coke","price":"20.00"},
+            {"item_id":"338","item_name":"Sprite (250 Ml)","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Sprite","price":"20.00"},
+            {"item_id":"339","item_name":"Fanta (250 Ml)","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Fanta","price":"20.00"},
+            {"item_id":"340","item_name":"Aloo Chat","category_name":"Light bites  (Veg)","is_veg":"1","description":"Aloo Chat","price":"69.00"},
+            {"item_id":"342","item_name":"Gobhi Paratha","category_name":"Snack  (Veg)","is_veg":"1","description":"Gobhi Paratha","price":"57.14"},
+            {"item_id":"343","item_name":"Extra Poori","category_name":"Rice/Paratha  (Veg)","is_veg":"1","description":"Enjoy our Banarasi style soft stuffed matar poori which is a treat to the tongue with every bite (1 Pcs)","price":"30.00"},
+            {"item_id":"346","item_name":"Thums Up (250 Ml)","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Thums Up (250 Ml)","price":"20.00"},
+            {"item_id":"348","item_name":"Aloo Tikki Chaat","category_name":"Snack  (Veg)","is_veg":"1","description":"Aloo Tikki Chaat","price":"34.00"},
+            {"item_id":"349","item_name":"Water Bottle 500ml","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Water Bottle 500ml","price":"10.00"},
+            {"item_id":"386","item_name":"Extra Cream Cheese","category_name":"Sides  (Veg)","is_veg":"1","description":"Extra Cream Cheese","price":"5.71"},
+            {"item_id":"518","item_name":"Egg Thali Combo","category_name":"New Year  (Veg)","is_veg":"1","description":"veg","price":"150.00"},
+            {"item_id":"1040","item_name":"Chole Wrap Jumbo","category_name":"Snack  (Veg)","is_veg":"1","description":"Chole Wrap Jumbo","price":"113.30"},
+            {"item_id":"517","item_name":"Cream Cheese","category_name":"Snack  (Veg)","is_veg":"1","description":"Cream Cheese","price":"15.00"},
+            {"item_id":"607","item_name":"Kashmiri Chicken Bowl (Regular)","category_name":"bowl (Non Veg)","is_veg":"0","description":"Kashmiri Chicken  Bowl (Regular)","price":"129.00"},
+            {"item_id":"529","item_name":"Kashmiri Chicken Bowl (NonVeg)","category_name":"bowl (Non Veg)","is_veg":"0","description":"Kashmiri Chicken Bowl (NonVeg)","price":"99.00"},
+            {"item_id":"514","item_name":"Peri Peri  Fries","category_name":"Snack  (Non Veg)","is_veg":"0","description":"Egg","price":"48.00"},
+            {"item_id":"437","item_name":"Egg Chicken Kaati Roll","category_name":"Starters (Non Veg)","is_veg":"0","description":"Chicken chunks dipped in creamy cheese and rolled in tortilla is an ode to Peshawari Food tales","price":"110.00"},
+            {"item_id":"441","item_name":"Samosa Chaat","category_name":"Snack  (Veg)","is_veg":"1","description":"samosa","price":"29.00"},
+            {"item_id":"442","item_name":"Ginger Tea","category_name":"New Year  (Veg)","is_veg":"1","description":"Ginger Tea","price":"40.00"},
+            {"item_id":"443","item_name":"Tea","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"tea","price":"25.00"},
+            {"item_id":"443","item_name":"Tea","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"tea","price":"25.00"},
+            {"item_id":"444","item_name":"Chole Bhature","category_name":"New Year  (Veg)","is_veg":"1","description":"Chole Bhature","price":"60.00"},
+            {"item_id":"454","item_name":"Gulab Jamun (1pcs)","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Chhui-mui Gulabjamun","price":"20.00"},
+            {"item_id":"459","item_name":"Extra Steamed Rice","category_name":"Starters (Non Veg)","is_veg":"0","description":"Extra Steamed Rice","price":"50.00"},
+            {"item_id":"463","item_name":"Gulab Jamun (14 Pcs)","category_name":"Dessets/Beverages (Veg)","is_veg":"1","description":"Gulab Jamun (14 Pcs)","price":"250.00"},
+            {"item_id":"1039","item_name":"Strawberry Phirni (1 KG)","category_name":"Dessets/Beverages (Veg)","is_veg":"1","description":"Strawberry Phirni (1 KG)","price":"230.00"},
+            {"item_id":"500","item_name":"Coolberg (Cranberry)","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Beat this summer with refreshing non alcoholic Cranberry flavoured beer","price":"90.00"},
+            {"item_id":"967","item_name":"Paneer Tikka Sandwich Combo","category_name":"Light bites  (Veg)","is_veg":"1","description":"Paneer Tikka Sandwich,Potato Wedges and Cold Drink","price":"100.00"},
+            {"item_id":"968","item_name":"Chicken Tikka Sandwich Combo","category_name":"Light bites (Non Veg)","is_veg":"0","description":"Chicken Tikka Sandwich, Potato Wedges and Cold Drink","price":"110.00"},
+            {"item_id":"499","item_name":"Coolberg (Peach)","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Beat this summer with refreshing non alcoholic peach flavoured beer","price":"90.00"},
+            {"item_id":"497","item_name":"Coolberg (Ginger)","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Beat this summer with refreshing non alcoholic Ginger flavoured beer","price":"90.00"},
+            {"item_id":"498","item_name":"Mint Mojito","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Beat this summer with refreshing non alcoholic mint flavoured beer","price":"90.00"},
+            {"item_id":"495","item_name":"Veg Combo","category_name":"Main course (Veg)","is_veg":"1","description":"Jeera Rice","price":"123.80"},
+            {"item_id":"488","item_name":"Coffee","category_name":"Desserts/Beverages  (Veg)","is_veg":"1","description":"Coffee","price":"25.00"},
+            {"item_id":"540","item_name":"Nizaam E Paneer Biryani","category_name":"Biryani  (Veg)","is_veg":"1","description":"Nizaam E Paneer Biryani","price":"205.00"},
+            {"item_id":"521","item_name":"Veg  Combo Meals","category_name":"Main course  (Veg)","is_veg":"1","description":"BTM","price":"119.00"},
+            {"item_id":"525","item_name":"NonVeg Combo Meals","category_name":"Main course (Non Veg)","is_veg":"0","description":"BTM","price":"139.00"},
+            {"item_id":"530","item_name":"Chicken Makhni Bowl (NonVeg)","category_name":"bowl (Non Veg)","is_veg":"0","description":"Chicken Makhni Bowl (NonVeg)","price":"99.00"},
+            {"item_id":"531","item_name":"Chicken Nawabi Bowl(NonVeg)","category_name":"bowl (Non Veg)","is_veg":"0","description":"Chicken Nawabi Bowl(NonVeg)","price":"99.00"},
+            {"item_id":"532","item_name":"Amritsari  Chicken Bowl(NonVeg)","category_name":"bowl (Non Veg)","is_veg":"0","description":"Amritsari  Chicken Bowl(NonVeg)","price":"99.00"},
+            {"item_id":"533","item_name":"Chicken Tikka Bowl  (NonVeg)","category_name":"bowl (Non Veg)","is_veg":"0","description":"Chicken tikka Bowl (NonVeg)","price":"99.00"},
+            {"item_id":"534","item_name":"Kashmiri Paneer Bowl(Veg)","category_name":"bowl  (Veg)","is_veg":"1","description":"Kashmiri Paneer Bowl(Veg)","price":"99.00"},
+            {"item_id":"535","item_name":"Paneer Makhni Bowl","category_name":"bowl  (Veg)","is_veg":"1","description":"Paneer Makhni Bowl","price":"99.00"},
+            {"item_id":"536","item_name":"Paneer Nawabi  Bowl(veg)","category_name":"bowl  (Veg)","is_veg":"1","description":"Nawabi Kofta Bowl(veg)","price":"99.00"},
+            {"item_id":"537","item_name":"Paneer Tikka Bowl(Veg)","category_name":"bowl  (Veg)","is_veg":"1","description":"Paneer Tikka Bowl(Veg)","price":"99.00"},
+            {"item_id":"538","item_name":"Amritsari Paneer Bowl(veg)","category_name":"bowl  (Veg)","is_veg":"1","description":"Amritsari paneer Bowl(veg)","price":"99.00"}
+        ],
+        };
+    }
+
+    render() {
+        const menu = this.state.dishes.map((dish) => {
+            return (
+              <div key={dish.item_id} className="detail">
+                  <Media body className="ml-5">
+                    <Media heading><b>Item Name: </b>{dish.item_name}</Media>
+                    <Media heading><b>Category: </b>{dish.category_name}</Media>
+                    <p><b>Description: &nbsp;</b>{dish.description}</p>
+                    <p><b>Price: &nbsp;</b>{dish.price}</p>
+                  </Media>
+              </div>
+            );
+        });
+
+        return (
+          <div className="container">
+            <div className="row">
+              <Media list>
+                  {menu}
+              </Media>
+            </div>
+          </div>
+        );
+    }
+}
+
+export default Menu;
